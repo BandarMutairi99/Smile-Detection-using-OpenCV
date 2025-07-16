bab849315f5a9e72e1bf378132919ce98993e29a
BandarMutairi99 <104208388+BandarMutairi99@users.noreply.github.com>
2025-07-16 18:35:59 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,40 @@
+import cv2
+
+# تحميل كواشف الوجوه والابتسامة
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+smile_cascade = cv2.CascadeClassifier("haarcascade_smile.xml")
+
+# فتح الكاميرا
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # كشف الوجوه
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x, y, w, h) in faces:
+        
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (255,0,0), 2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+
+        
+        smiles = smile_cascade.detectMultiScale(roi_gray, scaleFactor=1.8, minNeighbors=20)
+
+        for (sx, sy, sw, sh) in smiles:
+            cv2.rectangle(roi_color, (sx,sy), (sx+sw, sy+sh), (0,255,0), 2)
+            cv2.putText(frame, "Smiling 😊", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,255,0), 2)
+            break  
+
+    cv2.imshow("Smile Detection", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
